f31505f5765d609f40e028e348730901a695871b
turandon <mail@turandon.de>
2013-11-21 13:39:21 +0100

    completed A2

--- A2/lib/days.rb
+++ A2/lib/days.rb
@@ -11,6 +11,25 @@ DAY_SYM_ARR = [
   :SO
 ]
 
+ # index_to_day :: Int :: (index) ->? Int
+def index_to_day(index)
+  check_pre((
+    (index.int?)
+  ))
+
+  return index + 1
+end
+
+# day_to_index :: Int :: (day_num) ->? Int
+def day_to_index(day_num)
+  check_pre((
+    (day_num.int?)
+  ))
+
+  return day_num - 1
+end
+
+
 # day_num? :: Any :: (day) ->? Bool
 #
 # Test (
@@ -51,4 +70,179 @@ end
 # )
 def day?(day)
   return (day_num?(day) or day_sym?(day))
+end
+
+# day_num_to_day_sym :: Num :: (day_num) ->? Sym
+#
+# Test (
+#   (1) => :MO,
+#   (7) => :SO,
+#   (:MI) => ERR,
+#   ('stuff') => ERR
+# )
+def day_num_to_day_sym(day_num)
+  check_pre((
+    (day_num?(day_num))
+  ))
+
+  return DAY_SYM_ARR.at(day_to_index(day_num))
+end
+
+# day_sym_to_day_num :: Sym :: (day_sym) ->? Num
+#
+# Test (
+#   (:MO) => 1,
+#   (:SO) => 7,
+#   (3) => ERR,
+#   ('stuff') => ERR
+# )
+def day_sym_to_day_num(day_sym)
+  check_pre((
+    (day_sym?(day_sym))
+  ))
+
+  return index_to_day(DAY_SYM_ARR.find_index(day_sym))
+end
+
+# to_day_sym :: Any :: (day) ->? Sym
+#
+# Test (
+#   (4) => :DO,
+#   (:FR) => :FR,
+#   ('SO') => Err (my raise)
+# )
+
+def to_day_sym(day)
+  check_pre((
+    (day?(day))
+  ))
+
+  if day_sym?(day)
+    return day
+  elsif day_num?(day)
+    return day_num_to_day_sym(day)
+  end
+end
+
+# to_day_num :: Any :: (day) ->? Num
+#
+# Test (
+#   (4) => 4,
+#   (:FR) => 5,
+#   ('SO') => Err (my raise)
+# )
+def to_day_num(day)
+  check_pre((
+    (day?(day))
+  ))
+
+  if day_num?(day)
+    return day
+  elsif day_sym?(day)
+    return day_sym_to_day_num(day)
+  end
+end
+
+# day_num_succ :: Num :: (day_num) ->? Num
+#
+# Test (
+#   (1) => 2,
+#   (7) => 1,
+#   ('stuff') => ERR
+# )
+def day_num_succ(day_num)
+  check_pre((
+    (day_num?(day_num))
+  ))
+
+  day_num_succ = day_num.succ
+
+  return (day_num?(day_num_succ) ? day_num_succ : 1)
+end
+
+# day_num_pred :: Num :: (day_num) ->? Num
+#
+# Test (
+#   (1) => 7,
+#   (7) => 6,
+#   ('stuff') => ERR
+# )
+def day_num_pred(day_num)
+  check_pre((
+    (day_num?(day_num))
+  ))
+
+  day_num_pred = day_num.pred
+
+  return (day_num?(day_num_pred) ? day_num_pred : DAY_SYM_ARR.length)
+end
+
+# day_sym_succ :: Sym :: (day_sym) ->? sym
+#
+# Test (
+#   (:MO) => :DI,
+#   (:SO) => :MO,
+#   ('stuff') => ERR
+# )
+def day_sym_succ(day_sym)
+  check_pre((
+    (day_sym?(day_sym))
+  ))
+
+  index = day_to_index(day_num_succ(index_to_day(DAY_SYM_ARR.find_index(day_sym))))
+  return DAY_SYM_ARR.at(index)
+end
+
+# day_sym_pred :: Sym :: (day_sym) ->? sym
+#
+# Test (
+#   (:MO) => :SO,
+#   (:SO) => :SA,
+#   ('stuff') => ERR
+# )
+def day_sym_pred(day_sym)
+  check_pre((
+    (day_sym?(day_sym))
+  ))
+
+  index = day_to_index(day_num_pred(index_to_day(DAY_SYM_ARR.find_index(day_sym))))
+  return DAY_SYM_ARR.at(index)
+end
+
+# day_succ :: Any :: (day) ->? Any
+#
+# Test (
+#   (1) => 2,
+#   (:MO) => :DI,
+#   ('stuff') => ERR
+# )
+def day_succ(day)
+  check_pre((
+    (day?(day))
+  ))
+
+  if day_num?(day)
+    return day_num_succ(day)
+  elsif day_sym?(day)
+    return day_sym_succ(day)
+  end
+end
+
+# day_pred :: Any :: (day) ->? Any
+#
+# Test (
+#   (1) => 7,
+#   (:MO) => :SO,
+#   ('stuff') => ERR
+# )
+def day_pred(day)
+  check_pre((
+    (day?(day))
+  ))
+
+  if day_num?(day)
+    return day_num_pred(day)
+  elsif day_sym?(day)
+    return day_sym_pred(day)
+  end
 end
--- A2/test/days_test.rb
+++ A2/test/days_test.rb
@@ -8,39 +8,6 @@ require 'days'
 
 
 
-class DaysTest_num_sym < Test::Unit::TestCase
-
-  RT = RuntimeError
-
-  # Test (
-  #   1 => true,
-  #   2 => true,
-  #   8 => false,
-  #   'stuff' => false
-  # )
-  def test_day_num?
-    assert_equal(true, day_num?(1))
-    assert_equal(true, day_num?(2))
-
-    assert_equal(false, day_num?(8))
-    assert_equal(false, day_num?('stuff'))
-  end
-
- # Test (
- #   (:MO) => true,
- #   (:Di) => true,
- #   (:TO) => false,
- #   ('stuff') => false
- # )
- def test_day_sym?
-   assert_equal(true, day_sym?(:MO))
-   assert_equal(true, day_sym?(:DI))
-
-   assert_equal(false, day_sym?(:TO))
-   assert_equal(false, day_sym?('stuff'))
- end
-end
-
 class DaysTest < Test::Unit::TestCase
 
   RT = RuntimeError
@@ -63,4 +30,171 @@ class DaysTest < Test::Unit::TestCase
   end
 end
 
+class DaysTest_num_sym < Test::Unit::TestCase
 
+  RT = RuntimeError
+
+  # Test (
+  #   1 => true,
+  #   2 => true,
+  #   8 => false,
+  #   'stuff' => ERR
+  # )
+  def test_day_num?
+    assert_equal(true, day_num?(1))
+    assert_equal(true, day_num?(2))
+
+    assert_equal(false, day_num?(8))
+    assert_equal(false, day_num?('stuff'))
+  end
+
+ # Test (
+ #   (:MO) => true,
+ #   (:Di) => true,
+ #   (:TO) => false,
+ #   ('stuff') => ERR
+ # )
+ def test_day_sym?
+   assert_equal(true, day_sym?(:MO))
+   assert_equal(true, day_sym?(:DI))
+
+   assert_equal(false, day_sym?(:TO))
+   assert_equal(false, day_sym?('stuff'))
+ end
+end
+
+class DaysTest_to < Test::Unit::TestCase
+
+  RT = RuntimeError
+  
+  # Test (
+  #   (1) => :MO,
+  #   (7) => :SO,
+  #   (:TO) => ERR,
+  #   ('stuff') => ERR
+  # )
+  def test_day_num_to_day_sym
+    assert_equal(:MO, day_num_to_day_sym(1))
+    assert_equal(:SO, day_num_to_day_sym(7))
+
+    assert_raise(RT) { day_num_to_day_sym(:TO) }
+    assert_raise(RT) { day_num_to_day_sym('stuff') }
+  end
+
+  # Test (
+  #   (:MO) => 1,
+  #   (:SO) => 7,
+  #   (3) => ERR,
+  #   ('stuff') => ERR
+  # )
+  def test_day_sym_to_day_num
+    assert_equal(1, day_sym_to_day_num(:MO))
+    assert_equal(7, day_sym_to_day_num(:SO))
+
+    assert_raise(RT) { day_sym_to_day_num(3) }
+    assert_raise(RT) { day_sym_to_day_num('stuff') }
+  end
+
+  # Test (
+  #   (1) => :MO,
+  #   (:SO) => :SO,
+  #   ('stuff') => ERR
+  # )
+  def test_to_day_sym
+    assert_equal(:MO, to_day_sym(1))
+    assert_equal(:SO, to_day_sym(:SO))
+
+    assert_raise(RT) { to_day_sym('stuff') }
+  end
+
+  # Test (
+  #   (1) => 1,
+  #   (:SO) => 7,
+  #   ('stuff') => ERR
+  # )
+  def test_to_day_num
+    assert_equal(1, to_day_num(1))
+    assert_equal(7, to_day_num(:SO))
+
+    assert_raise(RT) { to_day_num('stuff') }
+  end
+end
+
+class DaysTest_succ_pred < Test::Unit::TestCase
+
+  RT = RuntimeError
+
+
+  # Test (
+  #   (1) => 2,
+  #   (7) => 1,
+  #   ('stuff') => ERR
+  # )
+  def test_day_num_succ
+    assert_equal(2, day_num_succ(1))
+    assert_equal(1, day_num_succ(7))
+
+    assert_raise(RT) { day_num_succ('stuff') }
+  end
+
+  # Test (
+  #   (1) => 7,
+  #   (7) => 6,
+  #   ('stuff') => ERR
+  # )
+  def test_day_num_pred
+    assert_equal(7, day_num_pred(1))
+    assert_equal(6, day_num_pred(7))
+
+    assert_raise(RT) { day_num_pred('stuff') }
+  end
+
+   # Test (
+  #   (:MO) => :DI,
+  #   (:SO) => :MO,
+  #   ('stuff') => ERR
+  # )
+  def test_day_sym_succ
+    assert_equal(:DI, day_sym_succ(:MO))
+    assert_equal(:MO, day_sym_succ(:SO))
+
+    assert_raise(RT) { day_sym_succ('stuff') }
+  end
+
+  # Test (
+  #   (:MO) => :SO,
+  #   (:SO) => :SA,
+  #   ('stuff') => ERR
+  # )
+  def test_day_sym_pred
+    assert_equal(:SO, day_sym_pred(:MO))
+    assert_equal(:SA, day_sym_pred(:SO))
+
+    assert_raise(RT) { day_sym_pred('stuff') }
+  end
+
+ # Test (
+  #   (1) => 2,
+  #   (:MO) => :DI,
+  #   ('stuff') => ERR
+  # )
+  def test_day_succ
+    assert_equal(2, day_succ(1))
+    assert_equal(:DI, day_succ(:MO))
+
+    assert_raise(RT) { day_succ('stuff') }
+  end
+
+  # Test (
+  #   (1) => 7,
+  #   (:MO) => :SO,
+  #   ('stuff') => ERR
+  # )
+  def test_day_pred
+    assert_equal(7, day_pred(1))
+    assert_equal(:SO, day_pred(:MO))
+
+    assert_raise(RT) { day_pred('stuff') }
+  end
+
+end
